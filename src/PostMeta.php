906a911840bbb1312a0e3ff2383aa05d775e7db6
Michael Kantor <mikenolimits@users.noreply.github.com>
2016-12-01 03:09:28 -0500

    Update PostMeta.php

--- src/PostMeta.php
+++ src/PostMeta.php
@@ -39,6 +39,61 @@ class PostMeta extends Model
      */
     protected $appends = ['value'];
 
+    public function isSerialized( $data, $strict = true ) {
+    // if it isn't a string, it isn't serialized.
+    if ( ! is_string( $data ) ) {
+        return false;
+    }
+    $data = trim( $data );
+    if ( 'N;' == $data ) {
+        return true;
+    }
+    if ( strlen( $data ) < 4 ) {
+        return false;
+    }
+    if ( ':' !== $data[1] ) {
+        return false;
+    }
+    if ( $strict ) {
+        $lastc = substr( $data, -1 );
+        if ( ';' !== $lastc && '}' !== $lastc ) {
+            return false;
+        }
+    } else {
+        $semicolon = strpos( $data, ';' );
+        $brace     = strpos( $data, '}' );
+        // Either ; or } must exist.
+        if ( false === $semicolon && false === $brace )
+            return false;
+        // But neither must be in the first X characters.
+        if ( false !== $semicolon && $semicolon < 3 )
+            return false;
+        if ( false !== $brace && $brace < 4 )
+            return false;
+    }
+    $token = $data[0];
+    switch ( $token ) {
+        case 's' :
+            if ( $strict ) {
+                if ( '"' !== substr( $data, -2, 1 ) ) {
+                    return false;
+                }
+            } elseif ( false === strpos( $data, '"' ) ) {
+                return false;
+            }
+            // or else fall through
+        case 'a' :
+        case 'O' :
+            return (bool) preg_match( "/^{$token}:[0-9]+:/s", $data );
+        case 'b' :
+        case 'i' :
+        case 'd' :
+            $end = $strict ? '$' : '';
+            return (bool) preg_match( "/^{$token}:[0-9.E-]+;$end/", $data );
+    }
+    return false;
+}
+
     /**
      * Gets the value.
      * Tries to unserialize the object and returns the value if that doesn't work.
@@ -48,12 +103,17 @@ class PostMeta extends Model
     public function getValueAttribute()
     {
         try {
+            if(!$this->isSerialized($this->meta_value))
+            {
+                return $this->meta_value;
+            }
             $value = unserialize($this->meta_value);
             // if we get false, but the original value is not false then something has gone wrong.
             // return the meta_value as is instead of unserializing
             // added this to handle cases where unserialize doesn't throw an error that is catchable
             return $value === false && $this->meta_value !== false ? $this->meta_value : $value;
         } catch (Exception $ex) {
+
             return $this->meta_value;
         }
     }
